9b4d310ffed42aaeb7ac8edbd1bb2592781f4bf0
Brian Larson <brla6495@colorado.edu>
2017-02-24 17:59:54 -0700

    Before my battery dies

---
+++ iOS/LuminousID/LuminousID/LogInViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LogInViewController.swift
+//  LuminousID
+//
+//  Created by Brian Larson on 2/24/17.
+//  Copyright © 2017 Garden Club. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+
+class LogInViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
