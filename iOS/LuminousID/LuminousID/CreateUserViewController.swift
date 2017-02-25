a9a6605045f57dc19e07e3efdde13b14f2fed724
Kevin-Rau <justkevinrau@gmail.com>
2017-02-24 17:49:17 -0700

    Logging in to Firebase

--- iOS/LuminousID/LuminousID/CreateUserViewController.swift
+++ iOS/LuminousID/LuminousID/CreateUserViewController.swift
@@ -12,17 +12,23 @@ import FirebaseAuth
 import FirebaseDatabase
 class CreateUserViewController: UIViewController {
 
+    @IBOutlet weak var userEmail: UITextField!
+    @IBOutlet weak var userPass: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        FIRAuth.auth()?.createUser(withEmail: "test2@provider.net", password: "sickPassword") { (user, error) in
-            // ...
-        }
-        // Do any additional setup after loading the view.
+               // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func createUser(_ sender: Any) {
+        FIRAuth.auth()?.createUser(withEmail: userEmail.text!, password: userPass.text!) { (user, error) in
+            // ...
+        }
+
     }
     
 
