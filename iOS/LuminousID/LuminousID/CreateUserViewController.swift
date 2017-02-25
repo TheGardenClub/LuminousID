031c36eff83da8787e3fd03efe7f080e7b60092a
Brian Larson <brla6495@colorado.edu>
2017-02-24 17:37:41 -0700

    Basic Auth Functionality

---
+++ iOS/LuminousID/LuminousID/CreateUserViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CreateUserViewController.swift
+//  LuminousID
+//
+//  Created by Brian Larson on 2/24/17.
+//  Copyright © 2017 Garden Club. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+class CreateUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        FIRAuth.auth()?.createUser(withEmail: "test2@provider.net", password: "sickPassword") { (user, error) in
+            // ...
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
