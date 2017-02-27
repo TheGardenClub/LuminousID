aad5a86ce704aecb3a36cd7b780ac36dfe10a249
Brian Larson <brla6495@colorado.edu>
2017-02-27 13:46:44 -0700

    Got the table view working

---
+++ iOS/LuminousID/LuminousID/FieldGuideTableViewController.swift
@@ -0,0 +1,70 @@
+//
+//  FieldGuideTableViewController.swift
+//  LuminousID
+//
+//  Created by Brian Larson on 2/27/17.
+//  Copyright © 2017 Garden Club. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
+
+class FieldGuideTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var fieldGuideTable: UITableView!
+    var myDict = [[String:AnyObject]]()
+    var speciesNames:[String] = []
+    var handle:FIRDatabaseHandle?
+    var ref:FIRDatabaseReference?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ref = FIRDatabase.database().reference().child("speciesid").child("field_guide").child("forbs")
+        ref?.observe(.value, with: { (snapshot) in
+            
+            if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot]
+            {
+                self.myDict = snapshots.flatMap { $0.value as? [String:AnyObject]}
+                for item in self.myDict{
+                    self.speciesNames.append(item["species_name"] as! String)
+                    self.fieldGuideTable.reloadData()
+                }
+
+            }
+        })
+        fieldGuideTable.reloadData()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return speciesNames.count
+    }
+    
+    
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "fieldGuideCell")
+        cell.textLabel?.text = self.speciesNames[indexPath.row]
+        return(cell)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
