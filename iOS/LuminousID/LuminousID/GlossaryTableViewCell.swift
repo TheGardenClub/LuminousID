163243cc7ebf13238faab8c8a0afc70fa52cafca
Brian Larson <brla6495@colorado.edu>
2017-04-10 13:20:12 -0600

    Added Glossary VC

---
+++ iOS/LuminousID/LuminousID/GlossaryTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  GlossaryTableViewCell.swift
+//  LuminousID
+//
+//  Created by Brian Larson on 4/10/17.
+//  Copyright © 2017 Garden Club. All rights reserved.
+//
+
+import UIKit
+
+class GlossaryTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var GlossaryImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
