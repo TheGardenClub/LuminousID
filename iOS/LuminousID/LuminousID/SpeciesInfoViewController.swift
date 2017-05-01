7c73aacff74c16c310c16edde668dd31b75a0fe4
Brian Larson <brla6495@colorado.edu>
2017-04-30 22:01:14 -0600

    Added the spacing and rearranged the attributes in species info

--- iOS/LuminousID/LuminousID/SpeciesInfoViewController.swift
+++ iOS/LuminousID/LuminousID/SpeciesInfoViewController.swift
@@ -23,10 +23,7 @@ class SpeciesInfoViewController: UIViewController {
     @IBOutlet weak var Label8: UILabel!
     @IBOutlet weak var Label9: UILabel!
     @IBOutlet weak var Label10: UILabel!
-    @IBOutlet weak var Label11: UILabel!
-    @IBOutlet weak var Label12: UILabel!
     @IBOutlet weak var Label13: UILabel!
-    @IBOutlet weak var Label14: UILabel!
 
     
     
@@ -72,17 +69,23 @@ class SpeciesInfoViewController: UIViewController {
             SpeciesImageView.image = UIImage(named: "Images/"+((speciesDict[0]["plant_code"]) as? String)! + ".jpg")
             if (speciesDict[0]["leaf_type"] as! String) == "deciduous"
             {
+                /*
                 Label1.text = "Species Name: " + (speciesDict[0]["species_name"] as! String)
-                Label2.text = "Common Name: " + (speciesDict[0]["common_name"] as! String)
-                Label3.text = "Synonyms: " + (speciesDict[0]["synonyms"] as! String)
+                */
+                Label1.text = "Common Name: " + (speciesDict[0]["common_name"] as! String)
+                Label2.text = "Synonyms: " + (speciesDict[0]["synonyms"] as! String)
+                /*
                 Label4.text = "Growth Form: " + (speciesDict[0]["growth_form"] as! String)
-                Label5.text = "Family Name: " + (speciesDict[0]["family_name"] as! String)
-                Label6.text = "Leaf Type: " + (speciesDict[0]["leaf_type"] as! String)
+                */
+                Label3.text = "Family: " + (speciesDict[0]["family_name"] as! String)
+                Label4.text = "Leaf Type: " + (speciesDict[0]["leaf_type"] as! String)
                 Label7.text = "Leaf Shape: " + (speciesDict[0]["leaf_shape"] as! String)
-                Label8.text = "Leaf Arrangement: " + (speciesDict[0]["leaf_arrangement"] as! String)
-                Label9.text = "Leaf Margin: " + (speciesDict[0]["leaf_margin"] as! String)
+                Label5.text = "Leaf Arrangement: " + (speciesDict[0]["leaf_arrangement"] as! String)
+                Label6.text = "Leaf Margin: " + (speciesDict[0]["leaf_margin"] as! String)
+                /*
                 Label10.text = "Cone: " + (speciesDict[0]["cone"] as! String)
-                Label11.text = "Photo Credit: " + (speciesDict[0]["photo_credit"] as! String)
+                */
+                Label8.text = "Photo Credit: " + (speciesDict[0]["photo_credit"] as! String)
                 /*
                 Label11.text = "Photo Credit: Brian Larson"
                 */
@@ -95,17 +98,21 @@ class SpeciesInfoViewController: UIViewController {
             }
             else if (speciesDict[0]["leaf_type"] as! String) == "needle" || (speciesDict[0]["leaf_type"] as! String) == "scale"
             {
+                /*
                 Label1.text = "Species Name: " + (speciesDict[0]["species_name"] as! String)
-                Label2.text = "Common Name: " + (speciesDict[0]["common_name"] as! String)
-                Label3.text = "Synonyms: " + (speciesDict[0]["synonyms"] as! String)
+                */
+                Label1.text = "Common Name: " + (speciesDict[0]["common_name"] as! String)
+                Label2.text = "Synonyms: " + (speciesDict[0]["synonyms"] as! String)
+                /*
                 Label4.text = "Growth Form: " + (speciesDict[0]["growth_form"] as! String)
-                Label5.text = "Family Name: " + (speciesDict[0]["family_name"] as! String)
-                Label6.text = "Leaf Type: " + (speciesDict[0]["leaf_type"] as! String)
+                */
+                Label3.text = "Family: " + (speciesDict[0]["family_name"] as! String)
+                Label4.text = "Leaf Type: " + (speciesDict[0]["leaf_type"] as! String)
                 Label7.text = "Needle Apex: " + (speciesDict[0]["needle_apex"] as! String)
-                Label8.text = "Needle Arrangement: " + (speciesDict[0]["needle_arrangement"] as! String)
-                Label9.text = "Needles Per Fascile: " + (speciesDict[0]["needle_per_fascile"] as! String)
-                Label10.text = "Cone: " + (speciesDict[0]["cone"] as! String)
-                Label11.text = "Photo Credit: " + (speciesDict[0]["photo_credit"] as! String)
+                Label5.text = "Needle Arrangement: " + (speciesDict[0]["needle_arrangement"] as! String)
+                Label6.text = "Needles Per Fascile: " + (speciesDict[0]["needle_per_fascile"] as! String)
+                Label8.text = "Cone: " + (speciesDict[0]["cone"] as! String)
+                Label9.text = "Photo Credit: " + (speciesDict[0]["photo_credit"] as! String)
  
                 /*
                 Label11.text = "Photo Credit: Brian Larson"
@@ -123,33 +130,46 @@ class SpeciesInfoViewController: UIViewController {
             SpeciesImageView.image = UIImage(named: "Images/"+((speciesDict[0]["plant_code"]) as? String)! + "_1.jpg")
             if (speciesDict[0]["family_name"] as! String) == "Cyperaceae (Sedges)"
             {
+                /*
                 Label1.text = "Species Name: " + (speciesDict[0]["species_name"] as! String)
-                Label2.text = "Common Name: " + (speciesDict[0]["common_name"] as! String)
-                Label3.text = "Synonyms: " + (speciesDict[0]["synonyms"] as! String)
+                */
+                Label1.text = "Common Name: " + (speciesDict[0]["common_name"] as! String)
+                Label2.text = "Synonyms: " + (speciesDict[0]["synonyms"] as! String)
+                /*
                 Label4.text = "Growth Form: " + (speciesDict[0]["growth_form"] as! String)
-                Label5.text = "Family Name: " + (speciesDict[0]["family_name"] as! String)
-                Label6.text = "Leaf Blade: " + (speciesDict[0]["leaf_blade"] as! String)
+                */
+                Label3.text = "Family: " + (speciesDict[0]["family_name"] as! String)
+                Label5.text = "Leaf Blade: " + (speciesDict[0]["leaf_blade"] as! String)
                 Label7.text = "Inflorescence: " + (speciesDict[0]["inflorescence"] as! String)
-                Label8.text = "Spike Color: " + (speciesDict[0]["spike_color"] as! String)
-                Label9.text = "Stem Cross Section: " + (speciesDict[0]["stem_cross_section"] as! String)
-                Label10.text = "Habitat: " + (speciesDict[0]["habitat"] as! String)
-                Label11.text = "Photo Credit: " + (speciesDict[0]["photo_credit"] as! String)
+                Label6.text = "Spike Color: " + (speciesDict[0]["spike_color"] as! String)
+                Label4.text = "Stem Cross Section: " + (speciesDict[0]["stem_cross_section"] as! String)
+                Label8.text = "Habitat: " + (speciesDict[0]["habitat"] as! String)
+                Label9.text = "Photo Credit: " + (speciesDict[0]["photo_credit"] as! String)
                 /*
                 Label11.text = "Photo Credit: Brian Larson"
                 */
-                Label13.text = "Notes: " + (speciesDict[0]["notes"] as! String)
+                if (speciesDict[0]["notes"] as! String) == "NA"{
+                    Label13.text = "Notes: " + "none"
+                }
+                else {
+                    Label13.text = "Notes: " + (speciesDict[0]["notes"] as! String)
+                }
             }
             else if (speciesDict[0]["family_name"] as! String) == "Juncaceae (Rushes)"
             {
+                /*
                 Label1.text = "Species Name: " + (speciesDict[0]["species_name"] as! String)
-                Label2.text = "Common Name: " + (speciesDict[0]["common_name"] as! String)
-                Label3.text = "Synonyms: " + (speciesDict[0]["synonyms"] as! String)
+                */
+                Label1.text = "Common Name: " + (speciesDict[0]["common_name"] as! String)
+                Label2.text = "Synonyms: " + (speciesDict[0]["synonyms"] as! String)
+                /*
                 Label4.text = "Growth Form: " + (speciesDict[0]["growth_form"] as! String)
-                Label5.text = "Family Name: " + (speciesDict[0]["family_name"] as! String)
-                Label6.text = "Leaf Blade: " + (speciesDict[0]["leaf_blade"] as! String)
-                Label7.text = "Stem Cross Section: " + (speciesDict[0]["stem_cross_section"] as! String)
-                Label8.text = "Habitat: " + (speciesDict[0]["habitat"] as! String)
-                Label9.text = "Photo Credit: " + (speciesDict[0]["photo_credit"] as! String)
+                */
+                Label3.text = "Family: " + (speciesDict[0]["family_name"] as! String)
+                Label5.text = "Leaf Blade: " + (speciesDict[0]["leaf_blade"] as! String)
+                Label4.text = "Stem Cross Section: " + (speciesDict[0]["stem_cross_section"] as! String)
+                Label6.text = "Habitat: " + (speciesDict[0]["habitat"] as! String)
+                Label7.text = "Photo Credit: " + (speciesDict[0]["photo_credit"] as! String)
                 /*
                 Label9.text = "Photo Credit: Brian Larson"
                 */
@@ -162,18 +182,22 @@ class SpeciesInfoViewController: UIViewController {
             }
             else if (speciesDict[0]["family_name"] as! String) == "Poaceae (grasses)"
             {
+                /*
                 Label1.text = "Species Name: " + (speciesDict[0]["species_name"] as! String)
-                Label2.text = "Common Name: " + (speciesDict[0]["common_name"] as! String)
-                Label3.text = "Synonyms: " + (speciesDict[0]["synonyms"] as! String)
+                */
+                Label1.text = "Common Name: " + (speciesDict[0]["common_name"] as! String)
+                Label2.text = "Synonyms: " + (speciesDict[0]["synonyms"] as! String)
+                /*
                 Label4.text = "Growth Form: " + (speciesDict[0]["growth_form"] as! String)
-                Label5.text = "Family Name: " + (speciesDict[0]["family_name"] as! String)
-                Label6.text = "Leaf Blade: " + (speciesDict[0]["leaf_blade"] as! String)
-                Label7.text = "Awns: " + (speciesDict[0]["awns"] as! String)
-                Label8.text = "Inflorescence: " + (speciesDict[0]["inflorescence"] as! String)
-                Label9.text = "Florets Per Spikelet: " + (speciesDict[0]["florets_per_spikelet"] as! String)
-                Label10.text = "Stem Cross Section: " + (speciesDict[0]["stem_cross_section"] as! String)
-                Label11.text = "Habitat: " + (speciesDict[0]["habitat"] as! String)
-                Label12.text = "Photo Credit: " + (speciesDict[0]["photo_credit"] as! String)
+                */
+                Label3.text = "Family: " + (speciesDict[0]["family_name"] as! String)
+                Label5.text = "Leaf Blade: " + (speciesDict[0]["leaf_blade"] as! String)
+                Label6.text = "Awns: " + (speciesDict[0]["awns"] as! String)
+                Label7.text = "Inflorescence: " + (speciesDict[0]["inflorescence"] as! String)
+                Label8.text = "Florets Per Spikelet: " + (speciesDict[0]["florets_per_spikelet"] as! String)
+                Label4.text = "Stem Cross Section: " + (speciesDict[0]["stem_cross_section"] as! String)
+                Label9.text = "Habitat: " + (speciesDict[0]["habitat"] as! String)
+                Label10.text = "Photo Credit: " + (speciesDict[0]["photo_credit"] as! String)
                 /*
                 Label12.text = "Photo Credit: Brian Larson"
                 */
